a04d1ddab751705410c16ca6172f380019876f5e
xv4t <fehiszaki@gmail.com>
2026-03-04 18:16:00 +0100

    Add files via upload

--- src/cli/handlers.rs
+++ src/cli/handlers.rs
@@ -1,13 +1,27 @@
-#![allow(unused_variables)]
-
 use super::commands::{Args, Subs};
+use super::super::db::queries;
 use std::sync::Arc;
 use sqlx::{Pool, Postgres};
+use futures::future::try_join_all;
 
-pub fn handle(cmd: Args, pool: Arc<Pool<Postgres>>){
+pub async fn handle(cmd: Args, pool: Arc<Pool<Postgres>>)-> Result<(), sqlx::Error>{
     match cmd.get_sub(){
         Subs::New { tags, title, description }=> {
+            //self explanatory..
+            let lower_case_tags: Vec<String>=tags.iter().map(|t| t.to_lowercase()).collect();
+            // what join_all eo
+            // 1. `join_all` wraps all futures into one combined future
+            // 2. `.await` hands that combined future to the **tokio runtime**
+            // 3. The runtime **polls all futures concurrently** within the current task
+            // 4. Each future runs until it hits its own `.await` (like a DB call or HTTP request), then **yields** back
+            // 5. The runtime keeps polling whichever futures are ready
+            // 6. Once **all** futures complete, `join_all` collects all results into a `Vec` and returns it
+            let in_db = try_join_all(lower_case_tags.iter().map(
+                |name| queries::check_for_tag_query(name, pool.clone())
+            )).await?.iter();
+            let new_tags: Vec<String>=lower_case_tags.iter().filter(|| );
 
+            Ok(())
         },
         Subs::Del { id, tags }=> {
             match (id, tags) {
@@ -22,15 +36,18 @@ pub fn handle(cmd: Args, pool: Arc<Pool<Postgres>>){
                     std::process::exit(1);
                 }
             }
+            Ok(())
         },
         Subs::Update { id, tags, title, description}=> {
             println!("  id: {id}");
             if let Some(t) = title       { println!("  new title      : {t}"); }
             if let Some(d) = description { println!("  new description: {d}"); }
             if let Some(ts) = tags       { println!("  new tags       : {}", ts.join(", ")); }
+            Ok(())
         },
         Subs::Get {}=> {
             println!("getting your logs from newest to oldest");
+            Ok(())
         }
     }
 }
--- src/db/queries.rs
+++ src/db/queries.rs
@@ -1,1 +1,26 @@
-// use sqlx;
+use sqlx::{Pool, Postgres};
+use std::sync::Arc;
+
+pub async fn new_log_query(title: &str, description: &str, pool: Arc<Pool<Postgres>>)
+-> Result<i32, sqlx::Error>{
+    let result: (i32,)= sqlx::query_as("INSERT INTO logs (title, content) VALUES ($1, $2) RETURNING id")
+    .bind(title)
+    .bind(description)
+    .fetch_one(&*pool).await?;
+
+    Ok(result.0)
+}
+
+pub async fn check_for_tag_query(tag: &str, pool: Arc<Pool<Postgres>>)-> Result<bool, sqlx::Error>{
+    let found: bool = sqlx::query_scalar("SELECT EXISTS (SELECT 1 FROM tags WHERE name = ?;")
+    .bind(tag).fetch_one(&*pool).await?;
+
+    Ok(found)
+}
+
+pub async fn new_tag_query(tag: &str, pool:Arc<Pool<Postgres>>)-> Result<i32, sqlx::Error>{
+    let result: (i32,)=sqlx::query_as("INSERT INTO tags (name) VALUES ($1) RETURNING id")
+    .bind(tag).fetch_one(&*pool).await?;
+
+    Ok(result.0)
+}
